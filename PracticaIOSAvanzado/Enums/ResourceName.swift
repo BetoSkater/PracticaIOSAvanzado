1661d37ab694b20e321f471d97f9b1126a85afe0
Alberto JR <albertojunquera.ra@gmail.com>
2023-02-15 11:58:20 +0100

    Network layer enum not committed

---
+++ PracticaIOSAvanzado/Enums/ResourceName.swift
@@ -0,0 +1,22 @@
+//
+//  ResourceName.swift
+//  PracticaIOSAvanzado
+//
+//  Created by Alberto Junquera Ramírez on 14/2/23.
+//
+
+import Foundation
+
+
+enum ResourceName: String{
+    //Added Assets
+    case loginBackground = "fondo2"
+    
+    //System Icons
+    case listIcon = "list.bullet.clipboard.fill"
+    case mapIcon = "map.fill"
+    
+    //Colours
+    case gokuBlue = "gokuBlue"
+    case gokuOrange = "gokuOrange"
+}
